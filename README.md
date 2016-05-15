306ae80ebbd05518c3bddeebddc774ab755a7512
HuntBao <gzooler@gmail.com>
2016-05-15 18:32:57 +0800

    commit files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# linkstatus
-Chrome extension: check link status of specified domain
+# Link Status
+Chrome extension: Check all links' status of your site
+
+
+# Thanks
+
+Jolly Icons Free: [http://jollyicons.com/](http://jollyicons.com/)
---
+++ src/background.js
@@ -0,0 +1,19 @@
+﻿//author @huntbao
+/*global chrome */
+
+'use strict'
+
+class BGPage {
+
+  constructor() {
+    this.addEvent()
+  }
+
+  static addEvent() {
+    chrome.browserAction.onClicked.addListener(function (tab) {
+      chrome.tabs.executeScript(null, {code: 'linkStatusChromeExt.create()'})
+    })
+  }
+}
+
+new BGPage()
